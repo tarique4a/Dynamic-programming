0d20fefc80834e4ab49cccf5481575fd18972af7
tarique4a <tarique.nitjsr77@gmail.com>
2022-06-29 23:22:55 +0530

    dp

--- longest_com_subseq.cpp
+++ longest_com_subseq.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestCommonSubsequence(string t1, string t2)
+int lcs(int x, int y, string a, string b)
 {
-    int n = t1.length() + 1;
-    int m = t2.length() + 1;
-    int dp[n][m];
-    for (int i = 0; i < n; ++i)
+    int dp[x + 1][y + 1];
+    for (int i = 0; i < x + 1; ++i)
     {
-        for (int j = 0; j < m; ++j)
+        for (int j = 0; j < y + 1; ++j)
+        {
             if (i == 0 || j == 0)
                 dp[i][j] = 0;
+        }
     }
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < x + 1; ++i)
     {
-        for (int j = 1; j < m; ++j)
+        for (int j = 1; j < y + 1; ++j)
         {
-            if (t1[i - 1] == t2[j - 1])
+            if (a[i - 1] == b[j - 1])
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             else
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }
-    return dp[n - 1][m - 1];
+    return dp[x][y];
 }
